6944359e071cdec36a55bf17b06660675f1372ab
Josh Pendragon <pendragon.josh@gmail.com>
2023-12-28 17:18:06 -0600

    refactor graph AM tests

--- data_structures/graphs/tests/test_adjacency_matrix.py
+++ data_structures/graphs/tests/test_adjacency_matrix.py
@@ -1,32 +1,49 @@
 import unittest
 
-
 from data_structures.graphs.graph_adjacency_matrix import GraphMatrix
 from data_structures.graphs.graph_adjacency_list import GraphList
 
+
 class TestAdjacencyMatrix(unittest.TestCase):
+
+    def test_get_directed_list(self):
+        test_list = GraphList(3)
+        self.matrix.add_edge(0, 1, True)
+        test_list.add_edge(0, 1, True)
+        g = self.matrix.get_list()
+
+        self.assertEqual(test_list.graph, g.graph)
+
+    def test_get_undirected_list(self):
+        test_list = GraphList(3)
+        self.matrix.add_edge(0, 1, False)
+        test_list.add_edge(0, 1, False)
+        g = self.matrix.get_list()
+
+        self.assertEqual(test_list.graph, g.graph)
+
     def setUp(self):
         self.matrix = GraphMatrix(3)
+        self.directed_graph = [
+            [0, 1, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
+        self.undirected_graph = [
+            [0, 1, 1],
+            [1, 0, 0],
+            [1, 0, 0]
+        ]
 
     def test_add_edge_directed(self):
         self.matrix.add_edge(0, 1, True)
 
-        self.assertEqual([
-            [0, 1, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ], self.matrix.graph, )
+        self.assertEqual(self.directed_graph, self.matrix.graph, )
 
     def test_add_edge_undirected(self):
         self.matrix.add_edge(0, 1, False)
         self.matrix.add_edge(0, 2, False)
-        self.matrix.add_edge(1, 2, False)
-
-        self.assertEqual(self.matrix.graph, [
-            [0, 1, 1],
-            [1, 0, 1],
-            [1, 1, 0]
-        ])
+        self.assertEqual(self.undirected_graph, self.matrix.graph, )
 
     def test_add_node(self):
         self.matrix.add_node(4)
@@ -45,22 +62,6 @@ class TestAdjacencyMatrix(unittest.TestCase):
         self.assertEqual([1, 2], self.matrix.get_neighbors(0))
         self.assertEqual([2], self.matrix.get_neighbors(1))
         self.assertEqual([], self.matrix.get_neighbors(2))
-
-    def test_get_directed_list(self):
-        test_list = GraphList(3)
-        self.matrix.add_edge(0, 1, True)
-        test_list.add_edge(0, 1, True)
-        g = self.matrix.get_list()
-
-        self.assertEqual(test_list.graph, g.graph)
-
-    def test_get_undirected_list(self):
-        test_list = GraphList(3)
-        self.matrix.add_edge(0, 1, False)
-        test_list.add_edge(0, 1, False)
-        g = self.matrix.get_list()
-
-        self.assertEqual(test_list.graph, g.graph)
 
 
 if __name__ == "__main__":
